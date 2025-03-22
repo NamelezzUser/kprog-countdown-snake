0f92b84c7b6082d4d97eb11c90619c0c8929f7cd
Enes K. <enes_kork@icloud.com>
2025-03-22 20:39:13 +0100

    copyright notice for sanke fx files

--- app/src/main/java/livesession/snake/javafx/SnakeDisplay.java
+++ app/src/main/java/livesession/snake/javafx/SnakeDisplay.java
@@ -1,3 +1,21 @@
+/**
+ * This file is part of countdown-snake-java.
+ *
+ * Copyright (c) 2025 Enes Korkmaz, Mira Wagner and Mae Weiland
+ *
+ * countdown-snake-java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package livesession.snake.javafx;
 
 import static livesession.snake.SnakeService.DEFAULT_NUMBER_OF_FOOD;
